dcf7dc800a4cd47b9c333e123a9c6596cf5139db
Vânia Mendonça <vania.pp.mendonca@gmail.com>
2014-05-12 10:43:33 +0100

    Start screen - how to play button

--- Assets/Scripts/GUI/StartScreen.cs
+++ Assets/Scripts/GUI/StartScreen.cs
@@ -16,7 +16,7 @@ public class StartScreen : MonoBehaviour {
     void Start() {
 
         backgroundRect = new Rect(0, 0, Screen.width, Screen.height);
-        menuRect = new Rect(Screen.width / 2 - 150, Screen.height / 3, 300, 170);
+        menuRect = new Rect(Screen.width / 2 - 150, Screen.height / 3, 300, 240);
         innerRect = new Rect(menuRect.x + 20, menuRect.y, menuRect.width - 40, menuRect.height - 50);
         playerName = "";
     }
@@ -31,31 +31,47 @@ public class StartScreen : MonoBehaviour {
         GUI.Box(menuRect, "");
         GUILayout.BeginArea(innerRect);
             GUILayout.Space(20);    
-            GUILayout.Label("The Game!", TitleStyle);
+            GUILayout.Label("Bookfinder!", TitleStyle);
             GUILayout.Space(20);
             GUILayout.BeginHorizontal();
             GUILayout.Label("Insert your name:", LabelStyle, GUILayout.Width(120), GUILayout.Height(22));
                 GUILayout.FlexibleSpace();
                 playerName = GUILayout.TextField(playerName, 125, GUILayout.Width(125));
             GUILayout.EndHorizontal();
-            GUILayout.Space(15);
+            GUILayout.Space(30);
+
             GUILayout.BeginHorizontal();
-                if (GUILayout.Button("Start game")) {
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("Start game", GUILayout.Width(150))) {
                     if (playerName != "") {
                         if(Definitions.Debug) Debug.Log("Let The Game begin...");
                         GameState.Instance.SetCurrentPlayer(playerName);                       
-                        //GameState.Instance.ChangeLevel(Definitions.Levels.GAME);
+                        //GameState.Instance.ChangeLevel(Definitions.Levels.GAME); // will be this one
                         GameState.Instance.ChangeLevel(Definitions.Levels.END);
                     }
                     else {
                         if (Definitions.Debug) Debug.Log("InsISTo que insira o seu nome!");
                     }                    
                 }
-                GUILayout.Space(20);
-                if (GUILayout.Button("Quit")) {
+                GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+            
+                if (GUILayout.Button("How to Play", GUILayout.Width(150))) {
+                    if (Definitions.Debug) Debug.Log("Vamos lá aprender!");                    
+                }
+                GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("Quit", GUILayout.Width(150))) {
                     if (Definitions.Debug) Debug.Log("Não desISTa!");
                     Application.Quit();
                 }
+                GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
         GUILayout.EndArea();
     }
