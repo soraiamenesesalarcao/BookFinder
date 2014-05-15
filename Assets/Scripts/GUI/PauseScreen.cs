a9567abba6c621da88cc79fe3e7d13f565d580c7
Vânia Mendonça <vania.pp.mendonca@gmail.com>
2014-05-15 10:23:30 +0100

    Help screens

--- Assets/Scripts/GUI/PauseScreen.cs
+++ Assets/Scripts/GUI/PauseScreen.cs
@@ -5,19 +5,19 @@ public class PauseScreen : MonoBehaviour {
 
     public GUIStyle TitleStyle;
 
-    private int rectOffset = 20;
-
     private Rect backgroundRect;
     private Rect menuRect;
     private Rect innerRect;
+    private Rect howRect;
 
     private Texture2D backgroundTex;
     private Texture2D menuTex;
 
     private Color backgroundColor;
-    private Color menuColor;
 
     private Definitions.GameMode mode;
+
+    private bool howToPlay = false;
 
     void Awake() {
 
@@ -26,11 +26,11 @@ public class PauseScreen : MonoBehaviour {
     void Start() {
 
         backgroundColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-        menuColor = new Color(0.3f, 0.3f, 0.3f, 0.5f);
 
         backgroundRect = new Rect(0, 0, Screen.width, Screen.height);
         menuRect = new Rect(Screen.width / 2 - 150, Screen.height / 3, 300, 220);
         innerRect = new Rect(menuRect.x + 20, menuRect.y, menuRect.width - 40, menuRect.height - 50);
+        howRect = new Rect(Screen.width / 2 - 300, 100, 600, 400);
 
         backgroundTex = GUIUtils.MakeTexture((int)backgroundRect.width, (int)backgroundRect.height, backgroundColor);
     }
@@ -44,53 +44,112 @@ public class PauseScreen : MonoBehaviour {
         if (mode == Definitions.GameMode.PAUSE) {
 
             GUI.DrawTexture(backgroundRect, backgroundTex);
-            GUI.Box(menuRect, "");
-            GUILayout.BeginArea(innerRect);
 
-            GUILayout.Space(20);
-            GUILayout.Label("Paused game", TitleStyle);
-            GUILayout.Space(20);
-
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button("Resume game", GUILayout.Width(150))) {
-                if (Definitions.Debug) Debug.Log("Back on track...");
-                GameState.Instance.SwitchPause();               
+            if (howToPlay) {
+                GUI.Box(howRect, "");
+                GUILayout.BeginArea(howRect);
+                GUILayout.Space(20);
+                GUILayout.Label("BookFinder: How to play", TitleStyle);
+                GUILayout.Space(20);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Molly and Wukong love reading. Unfortunately," +
+                                "Mukong was kidnapped by an evil monster called Groucho, " +
+                                "who also trapped Wukong's books in a maze. " +
+                                "Now Molly's mission is to find his books as fast as she can " +
+                                "and make it to the end of the maze in order to saver her friend.", GUILayout.Width(550));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.Space(20);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("ESC: Pause / Resume game", GUILayout.Width(300));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.Space(10);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("I: Show / hide inventory", GUILayout.Width(300));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.Space(10);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Arrows / W / A / S / D: Move Molly ", GUILayout.Width(300));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("(forward / left / backwards / right)", GUILayout.Width(300));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.Space(10);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Mouse motion: Rotate Molly ", GUILayout.Width(300));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.Space(20);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("Back", GUILayout.Width(150))) {
+                    if (Definitions.Debug) Debug.Log("Sair da ajuda");
+                    howToPlay = false;
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+                GUILayout.EndArea();
             }
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
+            else {
 
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
+                GUI.Box(menuRect, "");
+                GUILayout.BeginArea(innerRect);
 
-            if (GUILayout.Button("How to Play", GUILayout.Width(150))) {
-                if (Definitions.Debug) Debug.Log("Vamos lá aprender!");
+                GUILayout.Space(20);
+                GUILayout.Label("Paused game", TitleStyle);
+                GUILayout.Space(20);
+
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("Resume game", GUILayout.Width(150))) {
+                    if (Definitions.Debug) Debug.Log("Voltar");
+                    GameState.Instance.SwitchPause();
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("How to play", GUILayout.Width(150))) {
+                    if (Definitions.Debug) Debug.Log("Ajuda");
+                    howToPlay = true;
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("New game", GUILayout.Width(150))) {
+                    if (Definitions.Debug) Debug.Log("Começar de novo");
+                    GameState.Instance.FreezeGame(false);
+                    GameState.Instance.VisibleInventory = false;
+                    GameState.Instance.ChangeLevel(Definitions.Levels.START);
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("Quit", GUILayout.Width(150))) {
+                    if (Definitions.Debug) Debug.Log("Sair");
+                    Application.Quit();
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                GUILayout.EndArea();
             }
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button("New game", GUILayout.Width(150))) {
-                if (Definitions.Debug) Debug.Log("Here we go again...");
-                GameState.Instance.FreezeGame(false);
-                GameState.Instance.VisibleInventory = false;
-                GameState.Instance.ChangeLevel(Definitions.Levels.START);                
-            }
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button("Quit", GUILayout.Width(150))) {
-                if (Definitions.Debug) Debug.Log("Não desISTa!");
-                Application.Quit();
-            }
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-
-            GUILayout.EndArea();
-            
         }
     }
 
--- Assets/Scripts/GUI/StartScreen.cs
+++ Assets/Scripts/GUI/StartScreen.cs
@@ -10,13 +10,17 @@ public class StartScreen : MonoBehaviour {
     private Rect backgroundRect;
     private Rect menuRect;
     private Rect innerRect;
+    private Rect howRect;
+
     private string playerName;
+
+    private bool howToPlay = false;
 
  
     void Start() {
-
         backgroundRect = new Rect(0, 0, Screen.width, Screen.height);
         menuRect = new Rect(Screen.width / 2 - 150, Screen.height / 3, 300, 240);
+        howRect = new Rect(Screen.width / 2 - 300, 100, 600, 400);
         innerRect = new Rect(menuRect.x + 20, menuRect.y, menuRect.width - 40, menuRect.height - 50);
         playerName = "";
     }
@@ -27,52 +31,107 @@ public class StartScreen : MonoBehaviour {
     }
 
     void OnGUI() {
-        GUI.DrawTexture(backgroundRect, Background);
-        GUI.Box(menuRect, "");
-        GUILayout.BeginArea(innerRect);
-            GUILayout.Space(20);    
-            GUILayout.Label("Bookfinder!", TitleStyle);
+        GUI.DrawTexture(backgroundRect, Background);      
+
+        if (howToPlay) {
+            GUI.Box(howRect, "");
+            GUILayout.BeginArea(howRect);            
+            GUILayout.Space(20);
+            GUILayout.Label("BookFinder: How to play", TitleStyle);
+            GUILayout.Space(20);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Molly and Wukong love reading. Unfortunately," + 
+                            "Mukong was kidnapped by an evil monster called Groucho, " +
+                            "who also trapped Wukong's books in a maze. " + 
+                            "Now Molly's mission is to find his books as fast as she can " + 
+                            "and make it to the end of the maze in order to saver her friend.", GUILayout.Width(550));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(20);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("ESC: Pause / Resume game", GUILayout.Width(300));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("I: Show / hide inventory", GUILayout.Width(300));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Arrows / W / A / S / D: Move Molly ", GUILayout.Width(300));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("(forward / left / backwards / right)", GUILayout.Width(300));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Mouse motion: Rotate Molly ", GUILayout.Width(300));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(20);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Back", GUILayout.Width(150))) {
+                if (Definitions.Debug) Debug.Log("Sair da ajuda");
+                howToPlay = false;
+            }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
+        }
+        else {
+            GUI.Box(menuRect, "");
+            GUILayout.BeginArea(innerRect);
+            GUILayout.Space(20);
+            GUILayout.Label("BookFinder!", TitleStyle);
             GUILayout.Space(20);
             GUILayout.BeginHorizontal();
             GUILayout.Label("Insert your name:", LabelStyle, GUILayout.Width(120), GUILayout.Height(22));
-                GUILayout.FlexibleSpace();
-                playerName = GUILayout.TextField(playerName, 125, GUILayout.Width(125));
+            GUILayout.FlexibleSpace();
+            playerName = GUILayout.TextField(playerName, 125, GUILayout.Width(125));
             GUILayout.EndHorizontal();
             GUILayout.Space(30);
 
             GUILayout.BeginHorizontal();
-                GUILayout.FlexibleSpace();
-                if (GUILayout.Button("Start game", GUILayout.Width(150))) {
-                    if (playerName != "") {
-                        if(Definitions.Debug) Debug.Log("Let The Game begin...");
-                        GameState.Instance.SetCurrentPlayer(playerName);                       
-                        GameState.Instance.ChangeLevel(Definitions.Levels.GAME); // will be this one
-                        //GameState.Instance.ChangeLevel(Definitions.Levels.END);
-                    }
-                    else {
-                        if (Definitions.Debug) Debug.Log("InsISTo que insira o seu nome!");
-                    }                    
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Start game", GUILayout.Width(150))) {
+                if (playerName != "") {
+                    if (Definitions.Debug) Debug.Log("Jogar");
+                    GameState.Instance.SetCurrentPlayer(playerName);
+                    GameState.Instance.ChangeLevel(Definitions.Levels.GAME);
                 }
-                GUILayout.FlexibleSpace();
+            }
+            GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-                GUILayout.FlexibleSpace();
-            
-                if (GUILayout.Button("How to Play", GUILayout.Width(150))) {
-                    if (Definitions.Debug) Debug.Log("Vamos lá aprender!");                    
-                }
-                GUILayout.FlexibleSpace();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("How to play", GUILayout.Width(150))) {
+                if (Definitions.Debug) Debug.Log("Ajuda");
+                howToPlay = true;
+            }
+            GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-                GUILayout.FlexibleSpace();
-                if (GUILayout.Button("Quit", GUILayout.Width(150))) {
-                    if (Definitions.Debug) Debug.Log("Não desISTa!");
-                    Application.Quit();
-                }
-                GUILayout.FlexibleSpace();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Quit", GUILayout.Width(150))) {
+                if (Definitions.Debug) Debug.Log("Sair");
+                Application.Quit();
+            }
+            GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
-        GUILayout.EndArea();
+            GUILayout.EndArea();
+        }
     }
 }
