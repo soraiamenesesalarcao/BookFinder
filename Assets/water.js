528d586ef9f5d6aeaef88a8512782a0b7595b5ff
Soraia Meneses Alarcão <lua.svmac@gmail.com>
2014-05-28 14:11:39 +0100

    Added water (dont move)

---
+++ Assets/water.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+var scrollSpeed = 0.25;
+function FixedUpdate(){
+    var offset = Time.time * scrollSpeed;
+    renderer.material.mainTextureOffset = Vector2(0,-offset);
+    renderer.material.SetTextureOffset("_BumpMap", Vector2(0,-offset));
+}
